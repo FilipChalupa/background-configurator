4fc66aaae139016dc3b1014200086bc13aca24cc
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-23 23:44:03 +0100

    Animate page transition

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Konfigurátor: [intro.czechitas-podklady.cz](https://intro.czechitas-podklady.cz
 Ukázka: [Git 1](https://intro.czechitas-podklady.cz/slide.html?title=DA+Web%3A+Git+1&meta1=Filip+Chalupa&meta2=16.+z%C3%A1%C5%99%C3%AD+2020+%7C+17%3A30+-+20%3A30&meta3=Praha&icon=https%3A%2F%2Ficonape.com%2Fwp-content%2Ffiles%2Fni%2F64759%2Fpng%2Fgit-icon.png)
 
 ![ukázka](example.jpg)
+
+## Vývoj
+
+```bash
+npx live-server
+```
---
+++ main.js
@@ -0,0 +1,132 @@
+import { renderer } from './renderer.js'
+
+if (document.startViewTransition) {
+	async function getPageContent(url) {
+		const response = await fetch(url)
+		const text = await response.text()
+		return /<body[^>]*>([\w\W]*)<\/body>/.exec(text)[1]
+	}
+
+	async function onLinkNavigate(callback) {
+		navigation.addEventListener('navigate', (event) => {
+			const toUrl = new URL(event.destination.url)
+
+			if (location.origin !== toUrl.origin) {
+				return
+			}
+			event.intercept({
+				async handler() {
+					if (event.info === 'ignore') {
+						return
+					}
+					await callback({
+						toPath: toUrl.pathname,
+					})
+				},
+			})
+		})
+	}
+
+	onLinkNavigate(async ({ toPath }) => {
+		const content = await getPageContent(toPath)
+
+		document.startViewTransition(() => {
+			document.body.innerHTML = content
+			runPage()
+		})
+	})
+}
+
+const runOutputPage = () => {
+	const urlParameters = new URLSearchParams(window.location.search)
+	const title = urlParameters.get('title') ?? undefined
+	const meta1 = urlParameters.get('meta1') ?? undefined
+	const meta2 = urlParameters.get('meta2') ?? undefined
+	const meta3 = urlParameters.get('meta3') ?? undefined
+	const icon = urlParameters.get('icon') ?? undefined
+
+	const $output = document.querySelector('#output')
+	const { render } = renderer((url) => {
+		$output.src = url
+	})
+	render({
+		title,
+		meta1,
+		meta2,
+		meta3,
+		icon,
+	})
+}
+
+const runMainPage = () => {
+	const STORAGE_KEY = 'last-data'
+	const $form = document.querySelector('form')
+	const $preview = document.querySelector('#preview')
+	const $download = document.querySelector('#download')
+	const { render } = renderer((url) => {
+		$preview.src = url
+		$download.href = url
+	})
+
+	const persistFormData = () => {
+		const formData = new FormData($form)
+		const data = {}
+		for (let [name, value] of formData.entries()) {
+			data[name] = value
+		}
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+	}
+
+	const updatePreview = () => {
+		const formData = new FormData($form)
+		const parameters = Object.fromEntries(formData.entries())
+		render(parameters)
+	}
+
+	$form.addEventListener('submit', persistFormData)
+	$form.addEventListener(
+		'input',
+		(() => {
+			let timer
+			return () => {
+				persistFormData()
+				clearTimeout(timer)
+				timer = setTimeout(() => {
+					updatePreview()
+				}, 500)
+			}
+		})(),
+	)
+
+	try {
+		const data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {}
+		for (let [name, value] of Object.entries(data)) {
+			const $field = $form.querySelector(`[name="${name}"]`)
+			if ($field === null) {
+				continue
+			}
+			if ($field.type === 'checkbox') {
+				$field.checked = true
+			} else if (!$field.value) {
+				$field.value = value
+			}
+		}
+	} catch (error) {
+		console.error(error)
+	}
+	updatePreview()
+}
+
+const runPage = () => {
+	if (document.querySelector('#output')) {
+		runOutputPage()
+	} else if (document.querySelector('#main-page')) {
+		runMainPage()
+	}
+}
+
+runPage()
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('serviceWorker.js')
+}
